3760cc255e52a700da86b7e27cc709144a30ff2a
triruz <101410794+triruz@users.noreply.github.com>
2022-04-19 15:17:24 -0300

    Initial commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float altura;
+        public float edad;
+    
+    void Start()
+    {
+        if (edad < 1)
+        {
+            Debug.Log("Error");
+        }
+        else if (altura < 0)
+        {
+            Debug.Log("Error");
+        } else
+        {
+            if (altura > 1.2)
+            {
+                Debug.Log("Podes entrar");
+            }
+            else if (edad > 13)
+            {
+                Debug.Log("Podes entrar");
+            }
+            else
+            {
+                Debug.Log("No podes entrar");
+            }
+        }
+         
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
